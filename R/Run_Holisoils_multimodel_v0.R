ab80ef3ad117cf5c6ece8c63a2f2b8f3e965c365
elisabruni <72208269+elisabruni@users.noreply.github.com>
2022-09-13 10:37:23 +0200

    Update Run_Holisoils_multimodel_v0.R

--- R/Run_Holisoils_multimodel_v0.R
+++ R/Run_Holisoils_multimodel_v0.R
@@ -29,22 +29,30 @@ source(paste0(PATH_functions,"Forcing_data_test_run_v0.R"))
 source(paste0(PATH_functions,"Holisoils_Yasso07_v0.R"))
 source(paste0(PATH_functions,"Yasso07Model_fixed.R"))
 
-
 #---------------------
-#Other parameters that need to be defined
+#Other parameters that need to be defined (although it doesn't have to be the end-user)
 #Define spinup duration [years]
 spinup_length=1000
 #Define forward simulation length [years]
 #as number of data/time_step
 simulation_length=length(Temp_month$Date)*computation_time_step_fwd
-
+#Choose how many plots to draw in the same figure (num of rows, num of columns)
 par(mfrow=c(2,1))
-test_mm <-Call_MULTIMODEL(plot_figures=plot_figures,simulation_length=simulation_length, spinup_length=spinup_length,
-                          #computation_time_step_fwd=1, computation_time_step_spinup=1, tolti, perche ogni modello ha il suo gia predefinito
+#---------------------
+
+##############################
+#Call the multi-model ensemble
+###############################
+#This will launch the function defined in "Holisoils_multimodel_v0.R"
+test_mm <-Call_MULTIMODEL(plot_figures=plot_figures,
+                          simulation_length=simulation_length, spinup_length=spinup_length,
                           start_date_simulations=start_date_simulations_site,
                           temperature=Temp_month, precipitation=Precip_month, potential_evapotranspiration=Potevap_month,
-                          SOC_0=SOC_initial,C_input_spinup=Cinput_spinup,C_input_fwd=Cinput_fwd,clay_p=clay_site,silt_p=silt_site,carbonat_p=CaCO3_site, soil_thickness=soil_OM_thick,
-                          lignin_to_nitrogen=lignin_to_nitrogen_ratio,structural_in_lignin=structural_in_lignin_ratio,woodylittersize=woodylittersize_scalar,
+                          SOC_0=SOC_initial,
+                          C_input_spinup=Cinput_spinup,C_input_fwd=Cinput_fwd,
+                          clay_p=clay_site,silt_p=silt_site,carbonat_p=CaCO3_site, soil_thickness=soil_OM_thick,
+                          lignin_to_nitrogen=lignin_to_nitrogen_ratio,structural_in_lignin=structural_in_lignin_ratio,
+                          woodylittersize=woodylittersize_scalar,
                           CN_Ratio=CN_site, Bulk_Density=BD_site, WFPS=water_filled_pore_space, vswc=volumetric_soil_water_cont,CH4_Conc=CH4_conc_site,
                           historical_land_use=historical_LU,
                           decomposition_param_RothC=ksRothC,
