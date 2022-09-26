e0d3e86129806f3f7ed6d79c7b4883a33bd57da5
elisabruni <72208269+elisabruni@users.noreply.github.com>
2022-09-26 13:44:53 +0200

    Update Holisoils_RothC_v0.R

--- R/Holisoils_RothC_v0.R
+++ R/Holisoils_RothC_v0.R
@@ -18,7 +18,8 @@ Call_RothC<-function(plot_figures,
                      temperature, precipitation, potential_evapotranspiration,
                      SOC_0,C_input_spinup,C_input_fwd,clay_p,soil_thickness,
                      decomposition_param_RothC,
-                     t_spinup,t_fwd){
+                     t_spinup,t_fwd,
+                     spinupcheck, thresholdspin){
   
 
   #Convert variables in RothC
@@ -57,14 +58,13 @@ Call_RothC<-function(plot_figures,
   #Get SOC spinup
   CRothC_spinup=getC(RothC_spinup)
   
-   #Check that steady state is reached
-  datacheck=10 #number of years for which the steady state is sought
-  INIZ=CRothC_spinup[nrow(CRothC_spinup)-datacheck,] #initialize SOC pool values
-  for(row in 1:nrow(tail(CRothC_spinup,datacheck))){ 
-    pools_i = tail(CRothC_spinup,datacheck)[row,]
+  #Check that steady state is reached
+  INIZ=CRothC_spinup[nrow(CRothC_spinup)-spinupcheck,] #initialize SOC pool values
+  for(row in 1:nrow(tail(CRothC_spinup,spinupcheck))){ 
+    pools_i = tail(CRothC_spinup,spinupcheck)[row,]
     deltai = (INIZ-pools_i)/INIZ
     
-    if(all(deltai<0.1)){ #Check that SOC stock variation of each pool is <0.1 for all years
+    if(all(deltai<thresholdspin)){ #Check that SOC stock variation of each pool is <thresholdspin for all years
       print("spinup ok")}else{ #Otherwise stop and increase the spinup length
         print(paste(c("current delta is",deltai,collapse = " ")))
         stop("spinup length should be increased, current annual SOC variation is ")
